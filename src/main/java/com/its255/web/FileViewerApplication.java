8380e2b687f0369f0679fdba97350486a5b470b0
TS359985 <TS359985@MDC-CCE-CLD-169.bcbsa.com>
2026-02-06 07:52:13 -0600

    update service for multithread and caching

--- src/main/java/com/its255/web/FileViewerApplication.java
+++ src/main/java/com/its255/web/FileViewerApplication.java
@@ -2,7 +2,9 @@ package com.its255.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class FileViewerApplication {
     public static void main(String[] args) {
